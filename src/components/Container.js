b65f246600c17ee265e732c0ed08a1d1ee6a08d7
Jenna Blank <jennablank21@gmail.com>
2021-10-01 15:01:09 -0400

    super minimal Container component created

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,16 @@ import '../css/Container.css';
 function Container() {
   return (
     <div className="Container">
-      <p>This is the Brython contianer.</p>
+      <h1>Spinoza</h1>
+      <br />
+      
+      <textarea id="1" name="1" rows="4" cols="50">
+        This is the first textarea in the Container component.
+      </textarea>
+      <textarea id="2" name="2" rows="4" cols="50">
+        This is the second textarea in the Container component.
+      </textarea>
+      <button>Run</button>
     </div>
   );
 }
