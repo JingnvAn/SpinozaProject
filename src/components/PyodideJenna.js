fd1e30247db779900e4babb1db4053192c9fc8cc
Jenna Blank <jennablank21@gmail.com>
2021-10-22 11:27:56 -0400

    run functions loaded using useEffect, no button press needed anymore

--- src/components/PyodideJenna.js
+++ src/components/PyodideJenna.js
@@ -1,52 +1,40 @@
-import { useEffect, useRef, useState } from 'react';
-import {Helmet} from "react-helmet";
+import { useEffect, useRef, useState, useLayoutEffect } from 'react';
+import { Helmet } from "react-helmet";
 
 const globalThis = window;
 
-export default function Pyodide({
-  pythonCode,
-  loadingMessage = 'loading…',
-  evaluatingMessage = 'evaluating…'
-}) {
+export default function Pyodide({ pythonCode,
+                                  loadingMessage = 'loading…',
+                                  evaluatingMessage = 'evaluating…' }) {
+
   const indexURL = 'https://cdn.jsdelivr.net/pyodide/dev/full/'
   const pyodide = useRef(null)
   const [isPyodideLoading, setIsPyodideLoading] = useState(true)
   const [pyodideOutput, setPyodideOutput] = useState(evaluatingMessage)
+  const [componentMounted, setComponentMounted] = useState(false);
+  const [numRenders, setNumRenders] = useState(0);
 
-  // // load pyodide wasm module and initialize it
-  // useEffect(() => {
-  //   (async function () {
-  //     pyodide.current = await globalThis.loadPyodide({ indexURL })
-  //     setIsPyodideLoading(false)
-  //   })()
-  // }, [pyodide])
-  //
-  // // evaluate python code with pyodide and set output
-  // useEffect(() => {
-  //   if (!isPyodideLoading) {
-  //     const evaluatePython = async (pyodide, pythonCode) => {
-  //       try {
-  //         return await pyodide.runPython(pythonCode)
-  //       } catch (error) {
-  //         console.error(error)
-  //         return "Error evaluating Python code. See console for details."
-  //       }
-  //   }
-  //   (async function () {
-  //     setPyodideOutput(await evaluatePython(pyodide.current,
-  //     pythonCode))
-  //     })()
-  //   }
-  // }, [isPyodideLoading, pyodide, pythonCode])
+  useEffect(async () => {
+    console.log("in useEffect")
+    try {
+      await window.runitall()
+    } catch(e) {
+      console.log("Helmet has not executed scripts yet. Incrementing numRenders.")
+      setNumRenders(numRenders + 1)
+    }
+  }, [numRenders])
+
 
   return (
     <>
-      <Helmet>
-        <script src={'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js'} />
-      </Helmet>
+
       <div>
         Pyodide Output: {isPyodideLoading ? loadingMessage :
         pyodideOutput}
+        <button onClick={() => {
+          console.log('clicked the run run run button')
+          window.evaluatePython()
+          }}>Run run run</button>
       </div>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {Helmet} from "react-helmet";
 
 // Insert Component imports here
 //import App from './components/App';
 import Container from './components/Container';
 //import PyIDE from './components/pyodide'
-import Pyodide from './components/PyodideAll';
+// import Pyodide from './components/PyodideAll';
+import Pyodide from './components/PyodideJenna';
 
 // Insert CSS imports here
 import './index.css';
@@ -15,6 +17,10 @@ const pythonString = 'print()';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <script src={'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js'} />
+      <script src="testpy2.js"></script>
+    </Helmet>
     <Container />
     <Pyodide pythonCode={pythonString} />
   </React.StrictMode>,
