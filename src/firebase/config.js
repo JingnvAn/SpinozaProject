534b0f85edc452ec2b9c3e7b3b8742002183284b
Jingnu An <78512122+JingnvAn@users.noreply.github.com>
2021-12-09 12:47:39 -0500

    fix redirect to home (#14)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,7 @@ import firebase from "firebase";
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://www.example.com/finishSignUp?cartId=1234',
+  url: 'http://localhost:3000/Container',
   // This must be true.
   handleCodeInApp: true,
   iOS: {
